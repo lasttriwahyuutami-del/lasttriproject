a0313aa14786081f548cb26d753966a3a1316b06
lasttriwahyuutami-del <lasttriwahyuutami@gmail.com>
2026-05-22 14:13:21 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,8 @@ st.title("🎈 Aplikasi kimia")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+import datetime
+import streamlit as st
+
+d = st.date_input("When's your birthday", datetime.date(2019, 7, 6))
+st.write("Your birthday is:", d)
